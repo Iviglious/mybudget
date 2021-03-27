df3e7e7e6cf52362bc5a5a3bbf93cdc02e969233
Iviglious <ivo_shalev@yahoo.com>
2021-03-27 15:43:56 +0000

    Added helping shells

---
+++ build-functions.sh
@@ -0,0 +1,10 @@
+cd todo-src/
+
+cd addTodo; npm install; cd ..
+cd completeTodo; npm install; cd ..
+cd deleteTodo; npm install; cd ..
+cd getAllTodo; npm install; cd ..
+cd getTodo; npm install; cd ..
+cd updateTodo; npm install; cd ..
+
+cd ..
---
+++ redeploy.sh
@@ -0,0 +1,2 @@
+# Deploy previusly build/config
+sam deploy --config-file samconfig.toml --stack-name mybudget
